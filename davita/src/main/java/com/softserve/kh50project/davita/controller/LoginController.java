924b346fc28a9ed8eb03cf59b45fc083e67e3b01
Maya Nilga <maya.nilga@gmail.com>
2021-07-20 12:20:42 +0300

    created LoginController.java and login.html

---
+++ davita/src/main/java/com/softserve/kh50project/davita/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.softserve.kh50project.davita.controller;
+
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("login-form")
+    public String login() {
+        if (!SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            return "redirect:/home";
+        }
+        return "login";
+    }
+}
