c39a0d4df69c756a88d548362b1693fc7ba993c4
Maya Nilga <maya.nilga@gmail.com>
2021-07-22 00:03:37 +0300

    created JwtFilter, JwtProvider

--- davita/src/main/java/com/softserve/kh50project/davita/config/jwt/JwtFilter.java
+++ davita/src/main/java/com/softserve/kh50project/davita/config/jwt/JwtFilter.java
@@ -24,12 +24,17 @@ import static org.springframework.util.StringUtils.hasText;
 public class JwtFilter extends GenericFilterBean {
 
     public static final String AUTHORIZATION = "Authorization";
-
-    @Autowired
     private JwtProvider jwtProvider;
+    private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+    public void setJwtProvider(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+    @Autowired
+    public void setCustomUserDetailsService(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
