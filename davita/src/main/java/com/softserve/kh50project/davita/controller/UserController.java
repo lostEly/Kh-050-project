1472f2acaaa49f234e2d9a85b784bc6147fa005a
Maya Nilga <maya.nilga@gmail.com>
2021-07-26 12:38:47 +0300

    created UserController

---
+++ davita/src/main/java/com/softserve/kh50project/davita/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.softserve.kh50project.davita.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
