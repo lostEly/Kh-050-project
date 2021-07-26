c447b01c7749641f2fe90fe815625f65af0bef79
Maya Nilga <maya.nilga@gmail.com>
2021-07-26 17:53:23 +0300

    created class RoleServiceImpl

---
+++ davita/src/main/java/com/softserve/kh50project/davita/service/impl/RoleServiceImpl.java
@@ -0,0 +1,48 @@
+package com.softserve.kh50project.davita.service.impl;
+
+import com.softserve.kh50project.davita.model.Role;
+import com.softserve.kh50project.davita.repository.RoleRepository;
+import com.softserve.kh50project.davita.service.RoleService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class RoleServiceImpl implements RoleService {
+
+    private RoleRepository roleRepository;
+
+    @Override
+    public Role create(Role role) {
+        return roleRepository.save(role);
+    }
+
+    //TODO change NoSuchElementException to EntityNotFoundException
+    @Override
+    public Role readById(long id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(("Role with id=" + id + " does not exist!")));
+    }
+
+    @Override
+    public Role update(Role newRole, long id) {
+        newRole.setRoleId(id);
+        return roleRepository.save(newRole);
+    }
+
+    @Override
+    public void delete(long id) {
+        roleRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        List<Role> roles = roleRepository.findAll();
+        return roles.isEmpty() ? new ArrayList<>() : roles;
+    }
+}
