572f5b7661fab70d465c9d99e2f5667f66d24aa9
lostEly <krasnenkow29@gmail.com>
2021-07-26 17:57:22 +0300

    removed redundant import

--- davita/src/main/java/com/softserve/kh50project/davita/exceptions/ResourceNotFoundException.java
+++ davita/src/main/java/com/softserve/kh50project/davita/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,5 @@
 package com.softserve.kh50project.davita.exceptions;
 
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
