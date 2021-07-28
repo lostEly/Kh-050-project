5f9c5735410f4c2684c5aa5ef5af8058b30c7dff
tropik555 <yevhen.levchenko@nure.ua>
2021-07-29 01:42:35 +0300

    ResolveConflicts

--- davita/src/main/java/com/softserve/kh50project/davita/exceptions/ResourceNotFoundException.java
+++ davita/src/main/java/com/softserve/kh50project/davita/exceptions/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
@@ -15,5 +15,4 @@ public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message + "not found");
     }
-
 }
