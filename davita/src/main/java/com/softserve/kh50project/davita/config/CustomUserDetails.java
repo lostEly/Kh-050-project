8bcb451a686a0873fa591ee31b40535b3072a5e5
Maya Nilga <maya.nilga@gmail.com>
2021-07-30 13:43:52 +0300

    deleted comments

--- davita/src/main/java/com/softserve/kh50project/davita/config/CustomUserDetails.java
+++ davita/src/main/java/com/softserve/kh50project/davita/config/CustomUserDetails.java
@@ -1,24 +1,20 @@
 package com.softserve.kh50project.davita.config;
 
-import com.softserve.kh50project.davita.model.Role;
 import com.softserve.kh50project.davita.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
 
     private String login;
     private String password;
-    private Collection<? extends GrantedAuthority> grantedAuthorities;  ///56 min
+    private Collection<? extends GrantedAuthority> grantedAuthorities;
 
     public static CustomUserDetails fromUserToCustomUserDetails(User user) {
-
         CustomUserDetails c = new CustomUserDetails();
         c.login = user.getLogin();
         c.password = user.getPassword();
