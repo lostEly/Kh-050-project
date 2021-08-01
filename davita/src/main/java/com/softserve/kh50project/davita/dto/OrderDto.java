19526fb3e743579f3889b14e7e70ecd1ca91ccac
Igor Krivolutsky <igor.krivolut@gmail.com>
2021-08-02 02:48:55 +0300

    Another commit

--- davita/src/main/java/com/softserve/kh50project/davita/dto/OrderDto.java
+++ davita/src/main/java/com/softserve/kh50project/davita/dto/OrderDto.java
@@ -1,7 +1,5 @@
 package com.softserve.kh50project.davita.dto;
 
-import com.softserve.kh50project.davita.model.Order;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,17 +15,4 @@ public class OrderDto {
     private Long procedureId;
     private Long doctorId;
     private Long patientId;
-
-//    public OrderDto(Order order){
-//        if (order == null){
-//            throw new NullPointerException();
-//        }
-//        this.id = order.getOrderId();
-//        this.start = order.getStart().toString();
-//        this.finish = order.getFinish().toString();
-//        this.cost = order.getCost();
-//        this.procedureId = order.getProcedure().getProcedureId();
-//        this.doctorId = order.getDoctor().getUserId();
-//        this.patientId = order.getPatient().getUserId();
-//    }
 }
